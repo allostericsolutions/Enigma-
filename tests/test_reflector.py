94865c48141049d43ba3ade501f7397921e97588
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-09-04 00:01:39 -0400

    Update test_reflector.py

--- tests/test_reflector.py
+++ tests/test_reflector.py
@@ -1,1 +1,12 @@
+import unittest
+from enigma.reflector import Reflector
 
+class TestReflector(unittest.TestCase):
+    def test_reflect(self):
+        reflector = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
+        self.assertEqual(reflector.reflect('A'), 'Y')
+        self.assertEqual(reflector.reflect('B'), 'R')
+        self.assertEqual(reflector.reflect('C'), 'U')
+
+if __name__ == '__main__':
+    unittest.main()
