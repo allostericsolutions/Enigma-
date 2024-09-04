3c7004efc404381f59cbb5e3410f5429346da894
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-09-04 00:10:14 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from enigma.rotor import Rotor
 from enigma.reflector import Reflector
