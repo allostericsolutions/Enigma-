e47ad6311fd2a37f857f60e7ad46c72f8f1c751b
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-09-03 23:51:19 -0400

    Update enigma_machine.py

--- enigma/enigma_machine.py
+++ enigma/enigma_machine.py
@@ -1,1 +1,29 @@
+from .rotor import Rotor
+from .reflector import Reflector
+from .plugboard import Plugboard
 
+class EnigmaMachine:
+    def __init__(self, rotors, reflector, plugboard):
+        self.rotors = rotors
+        self.reflector = reflector
+        self.plugboard = plugboard
+
+    def encrypt_decrypt(self, text):
+        result = []
+        for char in text:
+            if char.isalpha():
+                char = char.upper()
+                char = self.plugboard.swap(char)
+                for rotor in self.rotors:
+                    char = rotor.forward(char)
+                char = self.reflector.reflect(char)
+                for rotor in reversed(self.rotors):
+                    char = rotor.backward(char)
+                char = self.plugboard.swap(char)
+                result.append(char)
+                for rotor in self.rotors:
+                    if not rotor.rotate():
+                        break
+            else:
+                result.append(char)
+        return ''.join(result)
