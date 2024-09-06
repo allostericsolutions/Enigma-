b0bf06fdf4208750cfe5e89d2e6ea1d8e821ccde
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-09-06 00:03:31 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -63,7 +63,7 @@ def main():
     if 'positions_line' in st.session_state:
         positions_line = st.session_state['positions_line']
     else:
-        positions_line = ["A", "A", "A"]
+        positions_line = [0, 0, 0]
     
     if 'plugboard_line' in st.session_state:
         plugboard_line = st.session_state['plugboard_line']
@@ -85,10 +85,9 @@ def main():
     # Initial rotor positions
     st.header("Initial Rotor Positions")
     rotor_positions = []
-    alphabet = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     for i, position in enumerate(positions_line):
-        position = st.selectbox(f"Initial position of rotor {i+1}", alphabet, index=alphabet.index(position), key=f"position_{i}")
-        selected_rotors[i].set_position(alphabet.index(position))
+        position = st.slider(f"Initial position of rotor {i+1}", 0, 25, position, key=f"position_{i}")
+        selected_rotors[i].set_position(position)
         rotor_positions.append(position)
 
     # Plugboard configuration
